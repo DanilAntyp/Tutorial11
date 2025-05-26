74f03c8a8d332fc70427aedba873d86f981bd165
mac <danila.antipenko@gmail.com>
2025-05-26 16:07:26 +0200

    added gitignore

--- Tutorial11/Program.cs
+++ Tutorial11/Program.cs
@@ -1,3 +1,7 @@
+using Tutorial11.Data;
+using Tutorial11.Services;
+using Microsoft.EntityFrameworkCore;
+
 namespace Tutorial11;
 
 public class Program
@@ -6,43 +10,31 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
-        builder.Services.AddAuthorization();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
 
-        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-        builder.Services.AddOpenApi();
+        builder.Services.AddControllers();
+
+        builder.Services.AddScoped<IDbService, DbService>();
+        builder.Services.AddScoped<IPatientService, PatientService>();
+        builder.Services.AddScoped<IPrescriptionService, PrescriptionService>();
+
+        builder.Services.AddDbContext<DatabaseContext>(options => 
+            options.UseSqlServer(builder.Configuration.GetConnectionString("Default"))
+        );
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
-            app.MapOpenApi();
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
 
         app.UseHttpsRedirection();
 
-        app.UseAuthorization();
-
-        var summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-            {
-                var forecast = Enumerable.Range(1, 5).Select(index =>
-                        new WeatherForecast
-                        {
-                            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                            TemperatureC = Random.Shared.Next(-20, 55),
-                            Summary = summaries[Random.Shared.Next(summaries.Length)]
-                        })
-                    .ToArray();
-                return forecast;
-            })
-            .WithName("GetWeatherForecast");
-
+        app.MapControllers();
         app.Run();
+
     }
 }
